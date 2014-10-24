aff18ce372941e22583afdecd23135273f236ecb
Joel <jmlassen@gmail.com>
2014-10-24 13:44:39 -0600

    Initial Commit

---
+++ src/dingo/Dingo.java
@@ -0,0 +1,18 @@
+package dingo;
+
+/**
+ *
+ * @author Joel Lassen
+ * @author Heidy
+ * @author Justin
+ */
+public class Dingo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
