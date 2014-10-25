64b381d61d1c1633fdf6dc0bc3258c4dd783e0da
Joel <jmlassen@gmail.com>
2014-10-24 19:46:54 -0600

    Added Test Package. Added ability to change where we are saying the log file to.

--- src/logging/Logger.java
+++ src/logging/Logger.java
@@ -14,11 +14,16 @@ import java.util.Calendar;
  */
 public class Logger {
     // Initialized when program starts
-    private static Logger instance = new Logger();
+    private static final Logger instance = new Logger();
+    // Directory where log file is getting saved
+    private File logDir;
     // Where the log is getting saved to
+    private String logFileNameStr;
     private File logFile;
     // The format of the timestamp
-    private DateFormat formatTimeStamp;
+    private final DateFormat formatTimeStamp;
+    // Whether the file directory has been validated
+    private boolean fileValidated = false;
     
     /**
      * Allows user to access the functions of the Logger throughout all their program.
@@ -33,9 +38,13 @@ public class Logger {
      * @param logString text to be logged
      */
     public void Log(String logString) {
-        // Get timestamp
-        String timeStamp = formatTimeStamp.format(Calendar.getInstance().getTime());
-        try {
+        try {    
+            // Check to see if file has been validated yet
+            if (!fileValidated) {
+                validateFile();
+            }
+            // Get timestamp
+            String timeStamp = formatTimeStamp.format(Calendar.getInstance().getTime());
             // Create the buffered writer and declare that we are appending the file
             BufferedWriter bw = new BufferedWriter(new FileWriter(logFile, true));
             // Log the timestamped message
@@ -49,30 +58,66 @@ public class Logger {
     }
     
     /**
+     * Get the log file.
+     * @return where the log file gets saved to
+     */
+    public File getLogFile() {
+        return logFile;
+    }
+    
+    /**
+     * Sets where the log file is going to be saved to.
+     * @param logFileStr name of log file (e.g. log.out) 
+     */
+    public void setLogFile(String logFileStr) {
+        this.logFileNameStr = logFileStr;
+        logFile = new File(logDir, logFileStr);
+        // Mark that file needs to be validated
+        fileValidated = false;
+    }
+    
+    /**
+     * Sets the directory where the log file is going to get saved to.
+     * @param logDir 
+     */
+    public void setLogDir(File logDir) {
+        this.logDir = logDir;
+        // Mark that file needs to be validated
+        fileValidated = false;
+    }
+    
+    /**
+     * Validates the save directory and file. Creates them if they do not exist.
+     * @throws Exception if file and directory cannot be created
+     */
+    private void validateFile() throws Exception {
+        // Check to see if directory exists
+        if (!logDir.isDirectory()) {
+            // Create the directory and sub directories
+            logDir.mkdirs();
+        }
+        // Check to see if file exists
+        if (!logFile.isFile()) {
+            // Create a new empty file
+            logFile.createNewFile();
+        }
+    }
+    
+    /**
      * Initialize where the log is getting saved to and timestamp format.
      */
     private Logger() {
         // Get the home for the user account
-        String logFileStr = System.getProperty("user.home");
+        String logDirStr = System.getProperty("user.home");
         // Get the directory seporator for the users system, and add our directory
-        logFileStr += File.separator + ".dingo" + File.separator;
-        String logDirStr = logFileStr += File.separator + "Logs" + File.separator;
-        // Add the name of the log file
-        logFileStr += "log.out";
-        // Initialize where the log file is going to get saved.
-        logFile = new File(logFileStr);
+        logDirStr += File.separator + ".dingo" + File.separator;
+        logDirStr += File.separator + "Logs" + File.separator;
         // Create save directory
-        File logDir = new File(logDirStr);
-        // Create file and director if not created yet
-        if (!logFile.isFile()) {
-            try {
-                if (!logDir.isDirectory()) {    // Check to make sure directory exsists
-                    logDir.mkdirs();
-                }
-                logFile.createNewFile();
-            } catch (Exception ex) {    // Do nothing if exception is thrown
-            }
-        }
+        logDir = new File(logDirStr);
+        // Initialize where the log file is going to get saved.
+        logFileNameStr = "log.out";
+        // Create save file
+        logFile = new File(logDirStr, logFileNameStr);
         // Set timestamp format, we should move string to a properties file
         formatTimeStamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS");
     }
