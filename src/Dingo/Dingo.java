f53bee8cb47f3ac0f6fec7b09ebbcd33eb722e05
jwhistine <justinwhistine@gmail.com>
2014-11-17 09:50:37 -0700

    I have implemented the create XML part in the appendLog() function. I will work on appending the information.

--- src/Dingo/Dingo.java
+++ src/Dingo/Dingo.java
@@ -1,7 +1,7 @@
 package Dingo;
 
 /**
- * 
+ *
  * @author Joel Lassen
  * @author Heidy
  * @author Justin
@@ -10,11 +10,16 @@ public class Dingo {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         new Dingo().run();
     }
 
-    private void run() {
+    private void run() throws Exception {
         DropboxService.start();
+        
+        Change change = new Change();
+        XMLService xml = new XMLService();
+        
+        xml.appendLog(change);
     }
 }
