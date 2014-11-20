be5a205a4a45930e96ca021ebcdab8e06038164f
jwhistine <justinwhistine@gmail.com>
2014-11-20 14:48:48 -0700

    The appengLog functions now writes and appends to the file.

--- src/Dingo/ChangeLogger.java
+++ src/Dingo/ChangeLogger.java
@@ -8,6 +8,9 @@ package Dingo;
 
 import java.util.List;
 import com.csvreader.CsvReader;
+import com.csvreader.CsvWriter;
+import java.io.File;
+import java.io.FileWriter;
 
 /**
  * @author mormon
@@ -25,12 +28,50 @@ public class ChangeLogger {
     }
    
     /**
-     * create an XML and add to it
+     * Once again, this is an example as to what we are going to do. It will
+     * be changed when needed.
+     * @param change
+     * @throws Exception 
      */
     public void appendLog(Change change) throws Exception {
         // use the write/append section under the bookmark(chrome)
+        String outputFile = "c:/output.csv";
+        
+        // check to see of the file has been created
+        boolean alreadyExists = new File(outputFile).exists();
+        
+        try {
+            // this will add the comma in between each value
+            CsvWriter output = new CsvWriter(new FileWriter(outputFile, true), ',');
+            
+            // make out the header for a file that does not exist
+            if (!alreadyExists) { 
+                output.write("id");
+                output.write("name");
+                output.endRecord();
+            }
+            
+            // if the header already exists, add information
+            output.write("3");
+            output.write("Joel");
+            output.endRecord();
+            
+            output.write("4");
+            output.write("Heidy");
+            output.endRecord();
+            
+            output.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
     
+    /**
+     * As of right now this is an example as to what we are going to do, but 
+     * it will not take much to change it.
+     * @param changes
+     * @throws Exception 
+     */
     public void readLog(Change changes) throws Exception {
         try { 
             CsvReader products = new CsvReader("c:/products.csv");
