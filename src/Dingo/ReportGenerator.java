a331e5b1662c23b89e05df45bdeb3e15864e518d
Heidy Cespedes <ces12001@byui.edu>
2014-12-11 10:11:10 -0700

    Implemented getLastChanges method and added writeToFile method

--- src/Dingo/ReportGenerator.java
+++ src/Dingo/ReportGenerator.java
@@ -6,6 +6,11 @@
 package dingo;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  *
@@ -14,13 +19,99 @@ import java.io.File;
 public class ReportGenerator {
     
     private int defaultChanges;
+    private List<Change> changes;
+
+    public ReportGenerator(int defaultChanges, List<Change> changes) {
+        this.defaultChanges = defaultChanges;
+        this.changes = changes;
+    }
+    
+    // Main is only for testing purposes, I'll delete it once this is fully
+    // implemented
+    public static void main(String args[]) {  
+        
+        List<Change> changes = new ArrayList<>();    
+        
+        Change change = new Change();
+        
+        for (int i = 0; i < 5; i++) {
+            change.setFilename("test");
+            change.setType("deletion");
+            change.setIsDirectory(false);
+            change.setModified(new Date());
+            change.setRevision(null);
+            changes.add(change);
+        }
+        
+        ReportGenerator report = new ReportGenerator(5, changes);
+        report.getLastChanges();
+        try {
+            report.writeToFile("c:/Users/HEIDY2016/Desktop/Report_Backup.txt");
+        } catch(FileNotFoundException ex) {
+            System.out.println("Sorry, the file was not found");
+        }
+    }      
     
     /**
      * @param changes 
      */
-    void getLastChanges(int changes) {
+    public void getLastChanges() {
+        System.out.println("FILENAME\t\t\t CHANGE\t\t\t CHANGE DATE\n");
         
+        // Check if there have been any changes recently
+        if (!changes.isEmpty()) {
+            // If the list of changes is smaller than the requested # of changes
+            if (defaultChanges >= changes.size()) {
+               for(Change ch:changes) {
+                   System.out.println(ch.getFilename() + "\t\t\t\t" + ch.getType() + "\t\t" + 
+                           ch.getDate().toString());
+               } 
+            } else {
+                for (int i = 0; i < defaultChanges; i++) {
+                    System.out.println(changes.get(i).getFilename() + "\t\t\t\t" +
+                            changes.get(i).getType() + "\t\t" + changes.get(i).getDate().toString());
+                }
+            }
+            
+        } else {
+            System.out.println("There are not recent changes to report");
+        }  
     }
+    
+    /**
+    * @param changesList
+    * @param file
+    * @throws java.io.FileNotFoundException
+    **/    
+    public void writeToFile(String file) throws FileNotFoundException {
+        PrintWriter writer = new PrintWriter(file);
+        String content = "";
+        System.out.println("Saving document " + file);
+        
+        writer.println("FILENAME\t\t\t CHANGE\t\t\t CHANGE DATE\n");
+        
+        // Check if there have been any changes recently
+        if (!changes.isEmpty()) {
+            // If the list of changes is smaller than the requested # of changes
+            if (defaultChanges >= changes.size()) {
+               for(Change ch:changes) {
+                   writer.println(ch.getFilename() + "\t\t\t\t" + ch.getType() + "\t\t" + 
+                           ch.getDate().toString());
+               } 
+            } else {
+                // Display only the number of requested changes, not the whole list
+                for (int i = 0; i < defaultChanges; i++) {
+                    writer.println(changes.get(i).getFilename() + "\t\t\t\t" +
+                            changes.get(i).getType() + "\t\t" + changes.get(i).getDate().toString());
+                }
+            }
+            
+        } else {
+            System.out.println("There are not recent changes to report");
+        }  
+        writer.close();
+    }
+    
     
     /**
      * 
