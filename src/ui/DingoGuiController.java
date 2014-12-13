acf465f08d379a0101fbda9380489f63618f3b8b
jwhistine <justinwhistine@gmail.com>
2014-12-13 12:38:46 -0700

    I have the GUI interacting. I will work on the other functionality

--- src/ui/DingoGuiController.java
+++ src/ui/DingoGuiController.java
@@ -7,7 +7,15 @@ package ui;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableView;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -15,6 +23,14 @@ import javafx.fxml.Initializable;
  * @author Joel Lassen <jmlassen at gmail.com>
  */
 public class DingoGuiController implements Initializable {
+    @FXML
+    private MenuItem closeGui;
+    @FXML
+    private TableView<?> taskTableView;
+    @FXML
+    private MenuItem startListen;
+    @FXML
+    private MenuItem stopListening;
 
     /**
      * Initializes the controller class.
@@ -23,5 +39,19 @@ public class DingoGuiController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void handleButtonEvent(ActionEvent event) {
+        System.exit(0);
+    }
+
+    @FXML
+    private void handleButtonEvent2(ActionEvent event) {
+        System.exit(0);
+    }
+
     
+    @FXML void handleButtonEvent4(ActionEvent event) {
+        System.exit(0);
+    }
 }
