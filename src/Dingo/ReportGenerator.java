217740704ec085c633989bb741bbf1169a358e70
Heidy Cespedes <ces12001@byui.edu>
2014-12-15 01:35:38 -0700

    Updated getLastChanges method

--- src/Dingo/ReportGenerator.java
+++ src/Dingo/ReportGenerator.java
@@ -25,11 +25,13 @@ public class ReportGenerator {
     private int defaultChanges;
     private List<Change> changes;
     private String file;
+    private String excelFile;
 
     public ReportGenerator(int defaultChanges, List<Change> changes) {
         this.defaultChanges = defaultChanges;
         this.changes = changes;
         this.file = "c:/Users/HEIDY2016/Desktop/Report_Backup.txt";
+        this.excelFile = "c:/Users/HEIDY2016/Desktop/lars.xls";
     }
     
     // Main is only for testing purposes, I'll delete it once this is fully
@@ -50,10 +52,10 @@ public class ReportGenerator {
             change.setModified(new Date());
             change.setRevision(null);
             changes.add(change);
-        }
-        
+        }        
         ReportGenerator report = new ReportGenerator(5, changes);
-        report.getLastChanges();        
+        report.getLastChanges(); 
+//        report.writeSpreadSheet();
     }      
     
     /**
@@ -102,6 +104,17 @@ public class ReportGenerator {
         writer.close(); 
     }
         
+     /**
+     * 
+     */
+//    void writeSpreadSheet() {
+//        Excel spreadSheet = new Excel();
+//        spreadSheet.setOutputFile(excelFile);               
+//        spreadSheet.write(changes);
+//        System.out
+//        .println("Please check the result file under c:/temp/lars.xls ");
+//    }
+    
     /**
      * 
      */
