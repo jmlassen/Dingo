8b6d7a71e88be791a91d34674bd86eea0af1b1cc
Heidy Cespedes <ces12001@byui.edu>
2014-12-15 13:04:18 -0700

    Implemented the Excel class

--- src/Dingo/Excel.java
+++ src/Dingo/Excel.java
@@ -24,36 +24,114 @@ import jxl.write.WritableWorkbook;
 import jxl.write.WriteException;
 import jxl.write.biff.RowsExceededException;
 
-/**
- *
- * @author HEIDY2016
- */
 public class Excel {
-    private WritableCellFormat timesBoldUnderline;
-    private WritableCellFormat times;
-    private String inputFile;
+  private WritableCellFormat timesBoldUnderline;
+  private WritableCellFormat times;
+  private String inputFile;
     
-    public Excel(String file) {
-        this.inputFile = file;
-    }
-    
-    public void setOutputFile(String inputFile) {
-        this.inputFile = inputFile;
-    }
-    
+  public void setOutputFile(String inputFile) {
+    this.inputFile = inputFile;
+  }
+
     /**
      *
      * @param changes
      * @throws IOException
      * @throws WriteException
      */
-    public void write() throws IOException, WriteException {
-        
+    public void write(List<Change> changes) throws IOException, WriteException {
+    File file = new File(inputFile);
+    WorkbookSettings wbSettings = new WorkbookSettings();
+
+    wbSettings.setLocale(new Locale("en", "EN"));
+
+    WritableWorkbook workbook = Workbook.createWorkbook(file, wbSettings);
+    workbook.createSheet("Report", 0);
+    WritableSheet excelSheet = workbook.getSheet(0);
+    createLabel(excelSheet);
+    createContent(excelSheet, changes);
+
+    workbook.write();
+    workbook.close();
+  }
+
+  public void createLabel(WritableSheet sheet)
+      throws WriteException {
+    // Lets create a times font
+    WritableFont times10pt = new WritableFont(WritableFont.TIMES, 10);
+    // Define the cell format
+    times = new WritableCellFormat(times10pt);
+    // Lets automatically wrap the cells
+    times.setWrap(true);
+
+    // create create a bold font with unterlines
+    WritableFont times10ptBoldUnderline = new WritableFont(WritableFont.TIMES, 10, WritableFont.BOLD, false,
+        UnderlineStyle.SINGLE);
+    timesBoldUnderline = new WritableCellFormat(times10ptBoldUnderline);
+    // Lets automatically wrap the cells
+    timesBoldUnderline.setWrap(true);
+
+    CellView cv = new CellView();
+    cv.setFormat(times);
+    cv.setFormat(timesBoldUnderline);
+    cv.setAutosize(true);
+
+    // Write a few headers
+    addCaption(sheet, 0, 0, "Filename");
+    addCaption(sheet, 1, 0, "Change");    
+    addCaption(sheet, 2, 0, "Change Date");
+
+  }
+
+  public void createContent(WritableSheet sheet, List<Change> changes) throws WriteException,
+      RowsExceededException { 
+    // Write a few number
+    for (int i = 0; i < changes.size(); i++) {
+      // First column
+      addLabel(sheet, 0, i + 1, changes.get(i).getFilename());
+      // Second column
+      addLabel(sheet, 1, i + 1, changes.get(i).getType());
+      // Third column
+      addLabel(sheet, 2, i + 1, changes.get(i).getDate().toString());
     }
+  }
+
+  public void addCaption(WritableSheet sheet, int column, int row, String s)
+      throws RowsExceededException, WriteException {
+    Label label;
+    label = new Label(column, row, s, timesBoldUnderline);
+    sheet.addCell(label);
+  }
+
+  public void addLabel(WritableSheet sheet, int column, int row, String s)
+      throws WriteException, RowsExceededException {
+    Label label;
+    label = new Label(column, row, s, times);
+    sheet.addCell(label);
+  }
+
+  public static void main(String[] args) throws WriteException, IOException {
     
-    public void createLabel(WritableSheet sheet) {
-        
-    }
-    
-    
+      List<Change> changes = new ArrayList<>();    
+                        
+        for (int i = 0; i < 5; i++) {  
+            Change change = new Change();
+            if ((i % 2) == 0) {                
+                change.setType("deletion  ");                 
+            } else {                
+                change.setType("alteration");                 
+            }
+            change.setFilename("test");
+            change.setIsDirectory(false);
+            change.setModified(new Date());
+            change.setRevision(null);
+            changes.add(change);
+        }
+      
+    Excel test = new Excel();
+    test.setOutputFile("c:/Users/HEIDY2016/Desktop/lars.xls");
+    test.write(changes);   
+    System.out
+        .println("Please check the result file under c:/temp/lars.xls ");
+  }  
 }
